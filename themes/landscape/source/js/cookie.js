6f2bd3ed86b671fe73c58c699073c69922320047
Bartosz <>
2019-04-01 12:58:12 +0200

    Add cookie consent

---
+++ themes/landscape/source/js/cookie.js
@@ -0,0 +1,17 @@
+window.addEventListener('load', function() {
+  window.cookieconsent.initialise({
+      'palette': {
+          'popup': {
+              'background': '#252e39'
+          },
+          'button': {
+              'background': '#14a7d0'
+          }
+      },
+      'theme': 'classic',
+      'position': 'bottom-right',
+      'content': {
+          'href': '/regulamin.pdf'
+      }
+  });
+});
