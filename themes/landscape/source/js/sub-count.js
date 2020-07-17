1d0661a318622e3cd267c4fec64057ceec4b32bd
Bartosz Pietrucha <pietrucha.bartosz@gmail.com>
2020-07-17 16:48:00 +0200

    Remove Mailchimp and add ActiveCampaign

--- themes/landscape/source/js/sub-count.js
+++ themes/landscape/source/js/sub-count.js
@@ -1,12 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
-  var value = 2600;
+  var value = 5210;
   var sidebarInfo = document.getElementById('sidebar-subscribe');
   var counter = document.getElementById('sub-count');
   counter.innerHTML = value;
 
   setTimeout(function() {
     sidebarInfo.classList.add('shake');
-  }, 1000);
+  }, 10000);
 
   var interval = setInterval(function() {
     counter.innerHTML = value += Math.floor(Math.random() * 4) + 1;
